c851c58f32a071ccb2c64f6d8b6bfe08b3d014b6
PrayudGurung <p_gurung186674@fanshaweonline.ca>
2024-04-07 17:31:06 -0400

    Firebase
    Firebase installed, configurations

---
+++ FinancialApp/src/utilities/firebase.js
@@ -0,0 +1,22 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore, collection, getDocs } from 'firebase/firestore/lite';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDOkrnWgwDUXdsajNlZinriobelcmPuWeU",
+  authDomain: "info--6132.firebaseapp.com",
+  projectId: "info--6132",
+  storageBucket: "info--6132.appspot.com",
+  messagingSenderId: "499120008272",
+  appId: "1:499120008272:web:22c8cf9bb6cd896a8e7555"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+const transactionCollection = collection(db, transactions)
+export const transactionsList= () => getDocs(transactionCollection)
